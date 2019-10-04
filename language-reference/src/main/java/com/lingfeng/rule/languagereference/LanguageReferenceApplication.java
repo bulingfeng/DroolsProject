d8fe18f15fc5c7896823d2f75bc33e6d89d02dd4
bulingfeng <413957294@qq.com>
2019-10-04 16:40:58 +0800

    提交规则引擎的项目模板

---
+++ language-reference/src/main/java/com/lingfeng/rule/languagereference/LanguageReferenceApplication.java
@@ -0,0 +1,13 @@
+package com.lingfeng.rule.languagereference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageReferenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanguageReferenceApplication.class, args);
+    }
+
+}
